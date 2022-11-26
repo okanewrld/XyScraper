7d35c6638111805a318a610e1f70e00cac0e5183
okanewrld <118990889+okanewrld@users.noreply.github.com>
2022-11-26 01:28:25 -0800

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,39 @@
+
+import os
+import time
+from colored import fg, attr
+g = fg('#875cd3')
+
+y = fg('#ffffff')
+os.system(f'cls & mode 85,20 & title Proxy Scraper [Created By Okane]')
+
+
+
+print(f"""
+                                   {g}██   ██ ██    ██ 
+                                   {y} ██ ██   ██  ██  
+                                   {g}  ███     ████   
+                                   {y} ██ ██     ██    
+                                   {g}██   ██    ██    
+                                   {y}
+                        - https://github.com/okanewrld
+                        - https://youtube.com/@okanewrld
+""")
+time.sleep(2)
+print(f"                            {g}Scraping Proxies For You...")
+time.sleep(3)
+print("")
+os.system(
+        "curl https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt -o proxies.txt"
+    )
+os.system('cls')
+print("")
+print(f"""
+{y}
+			        Check : proxies.txt
+
+			   Thanks For Using Okane's TOOLS
+
+			      You May Exit The Program!
+""")
+time.sleep(69)
